daa441cd095b837967a1ffe0b258afae824709c2
Vlad Midler <midlervlad@mail.ru>
2025-11-06 19:30:30 +0700

    Введена система атаки врага

---
+++ Assets/Project Resources/Scripts/Enemy/EnemyAttack.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using UnityEngine;
+
+public class EnemyAttack : MonoBehaviour
+{
+    [SerializeField] private Weapon _weapon;
+
+    [SerializeField] private float _waitTimeToAttack;
+
+    private WaitForSeconds _delay;
+    private Coroutine _coroutine;
+
+    private void Awake()
+    {
+        _delay = new WaitForSeconds(_waitTimeToAttack);
+    }
+
+    private void FixedUpdate()
+    {
+        if (_weapon.CanAttack)
+        {
+            if (_coroutine == null)
+            {
+                _coroutine = StartCoroutine(AttackTarget());
+            }
+        }
+
+        if (_weapon.CanAttack == false)
+        {
+            if (_coroutine != null)
+            {
+                StopCoroutine(_coroutine);
+
+                _coroutine = null;
+            }
+        }
+    }
+
+    private IEnumerator AttackTarget()
+    {
+        while (enabled)
+        {
+            _weapon.Attack();
+
+            yield return _delay;
+        }
+    }
+}
